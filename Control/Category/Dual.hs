edcb9da88533a8c2541ce07ce8bc3f239a517c2d
M Farkas-Dyck <strake888@gmail.com>
2017-11-23 03:44:21 -0800

    Read2, Show2

--- Control/Category/Dual.hs
+++ Control/Category/Dual.hs
@@ -2,7 +2,7 @@
 
 module Control.Category.Dual where
 
-import Prelude (Eq, Ord, Read, Show, Bounded)
+import Prelude (Eq, Ord, Read, Show, Bounded, ($))
 
 import Control.Category
 import Data.Functor.Classes
@@ -18,3 +18,11 @@ instance Category k => Category (Dual k) where
 
 instance Eq2 k => Eq2 (Dual k) where liftEq2 f g (Dual x) (Dual y) = liftEq2 g f x y
 instance Ord2 k => Ord2 (Dual k) where liftCompare2 f g (Dual x) (Dual y) = liftCompare2 g f x y
+
+instance Read2 k => Read2 (Dual k) where
+    liftReadsPrec2 arp arl brp brl =
+        readsData $ readsUnaryWith (liftReadsPrec2 brp brl arp arl) "Dual" Dual
+
+instance Show2 k => Show2 (Dual k) where
+    liftShowsPrec2 asp asl bsp bsl n =
+        showsUnaryWith (liftShowsPrec2 bsp bsl asp asl) "Dual" n . dual
