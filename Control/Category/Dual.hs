8a28fec445b2115abef67e668a6c417e1558e689
M Farkas-Dyck <strake888@gmail.com>
2017-11-23 01:37:15 -0800

    Start

---
+++ Control/Category/Dual.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module Control.Category.Dual where
+
+import Prelude (Eq, Ord, Read, Show, Bounded)
+
+import Control.Category
+import Data.Functor.Classes
+import Data.Semigroup (Semigroup)
+import Data.Monoid (Monoid)
+
+newtype Dual k a b = Dual { dual :: k b a }
+  deriving (Eq, Ord, Read, Show, Bounded, Semigroup, Monoid)
+
+instance Category k => Category (Dual k) where
+    id = Dual id
+    Dual f . Dual g = Dual (g . f)
+
+instance Eq2 k => Eq2 (Dual k) where liftEq2 f g (Dual x) (Dual y) = liftEq2 g f x y
+instance Ord2 k => Ord2 (Dual k) where liftCompare2 f g (Dual x) (Dual y) = liftCompare2 g f x y
