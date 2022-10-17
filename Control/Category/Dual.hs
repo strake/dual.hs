5c2b51a3d8166b2435c555fa54f291d525c4a6b3
Topsii <tobiashaslop@hotmail.de>
2022-10-18 00:18:14 +0200

    Add missing Functor, Foldable and Traversable instances
    Adresses haskell/core-libraries-committee#91 and haskell/core-libraries-committee#93

--- Control/Category/Dual.hs
+++ Control/Category/Dual.hs
@@ -1,16 +1,18 @@
 {-# LANGUAGE DerivingVia #-}
 module Control.Category.Dual where
 
-import Prelude (Eq, Ord, Read, Show, Bounded, ($))
+import Prelude (Eq, Ord, Read, Show, Bounded, ($), const, Applicative(..))
 
 import Control.Category
+import Data.Foldable
+import Data.Traversable
 import Data.Bifunctor
 import Data.Bifoldable
 import Data.Bitraversable
 import Data.Functor
 import Data.Functor.Classes
 import Data.Semigroup (Semigroup)
-import Data.Monoid (Monoid)
+import Data.Monoid (Monoid(..))
 
 newtype Dual k a b = Dual { dual :: k b a }
   deriving (Eq, Ord, Read, Show)
@@ -30,6 +32,15 @@ instance Read2 k => Read2 (Dual k) where
 instance Show2 k => Show2 (Dual k) where
     liftShowsPrec2 asp asl bsp bsl n =
         showsUnaryWith (liftShowsPrec2 bsp bsl asp asl) "Dual" n . dual
+
+instance Bifunctor k => Functor (Dual k b) where
+    fmap = bimap id
+
+instance Bifoldable k => Foldable (Dual k b) where
+    foldMap = bifoldMap mempty
+
+instance Bitraversable k => Traversable (Dual k b) where
+    traverse = bitraverse pure
 
 instance Bifunctor k => Bifunctor (Dual k) where
     bimap f g = Dual . bimap g f . dual
